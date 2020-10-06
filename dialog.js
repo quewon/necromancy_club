8b84eed69e0f8607eb3766a55c0c59ab35dfebbd
electriclantern <quewon.chin@gmail.com>
2020-10-06 14:34:48 +0900

    a

---
+++ dialog.js
@@ -0,0 +1,16 @@
+var dialog = {
+	LOG001:
+`necromancy club rules
+1. no practicing necromancy alone (at least three people or more!)
+2. no talking about necromancy outside the club
+3. always share new discoveries, no matter how small!!
+4. come to the meeting every thursday, after school, at the benches behind the courtyard`
+	,
+
+	LOG002:
+`necromancy club rules [updated august 7, 2010]
+1. no practicing necromancy`
+	,
+
+
+}
---
+++ script.js
@@ -0,0 +1,75 @@
+var screens = {
+	log: document.getElementById("log"),
+	files: document.getElementById("files")
+}
+
+function screen(screen_name) {
+	let keys = Object.keys(screens)
+	let length = keys.length;
+
+	for (let i=0; i<length; i++) {
+		if (screen_name != keys[i]) {
+			screens[keys[i]].style.display = 'none';
+		}
+	}
+
+	screens[screen_name].style.display = 'flex';
+}
+
+function log(t) {
+	type.text = t.split(/(?:\r\n|\r|\n)/g);;
+	type.timer = 0;
+	type.line = 0;
+
+	type.area.innerHTML = "";
+	typer();
+}
+
+var type = {
+	text: [],
+	timer: 0,
+	line: 0,
+	speed: 40,
+	cursor: "_",
+	cursor_visible: false,
+	break_timer: 0,
+	break_length: 9, //must be an odd number
+	end_of_line: false,
+	area: document.getElementById("logtext")
+}
+function typer() {
+	let { text, timer, line, speed, cursor, cursor_visible, break_timer, break_length, end_of_line, area } = type;
+
+	if (end_of_line) {
+		if (cursor_visible) {
+			area.innerHTML = area.innerHTML.slice(0,-1);
+		} else {
+			area.innerHTML += cursor;
+		}
+		type.cursor_visible = !cursor_visible;
+
+		if (break_timer >= break_length) {
+			type.break_timer = 0;
+			type.timer = 0;
+			type.line++;
+			type.end_of_line = false;
+			area.innerHTML += "<br />"
+		} else {
+			type.break_timer++;
+		}
+
+		setTimeout(typer, speed)
+	}
+
+	if (timer <= text[line].length) {
+		area.innerHTML += text[line].charAt(timer);
+		type.timer++;
+		if (timer == text[line].length) { type.end_of_line = true }
+		setTimeout(typer, speed)
+	}
+}
+
+//initialization
+
+screen('log');
+log(dialog.LOG001);
