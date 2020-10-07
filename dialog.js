97ee058afe9787faf8bff60bf3b4a143f88a61c1
electriclantern <quewon.chin@gmail.com>
2020-10-08 00:34:50 +0900

    .

--- dialog.js
+++ dialog.js
@@ -1,16 +1,13 @@
 var dialog = {
 	LOG0:
-`necromancy club rules
-1. no practicing necromancy alone (at least three people or more!)
-2. no talking about necromancy outside the club
-3. always share new discoveries, no matter how small!!
-4. come to the meeting every thursday, after school, at the benches behind the courtyard`
+`you are the first person to arrive at the club meeting. which is funny, because you're ten minutes late.
+
+then again, it's been five years since the last meeting. taking a seat on a familiar plastic bench, you look around, taking in the view of your middle school campus covered in snow. you might be more nostalgic if <a onclick="log(dialog.LOG1)">the news</a> wasn't so grave.`
 	,
 
 	LOG1:
-`necromancy club rules [updated august 7, 2010]
-1. no practicing necromancy`
+`a member of the necromancy club has <a onclick="log(dialog.LOG2)">died</a>.`
 	,
-
-
+	LOG2:
+`another one, to be clear. it's as tragic as it is ironic. the necromancy club has never stopped haunting you, not after you moved away, not now that you're an adult. so it wasn't all that shocking, when you read <a onclick="browse('email.com')">the invitation</a>. almost like you had been anticipating something like this.`
 }
--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+// screens
+
 var screens = {
 	log: document.getElementById("log"),
 	browser: document.getElementById("browser")
 }
+var current_screen;
 
 function screen(screen_name) {
 	let keys = Object.keys(screens)
@@ -13,63 +16,11 @@ function screen(screen_name) {
 		}
 	}
 
-	screens[screen_name].style.display = 'flex';
+	screens[screen_name].style.display = 'block';
+	current_screen = screen_name;
 }
 
-function log(t) {
-	type.text = t.split(/(?:\r\n|\r|\n)/g);;
-	type.timer = 0;
-	type.line = 0;
-
-	type.area.innerHTML = "";
-	typer();
-}
-
-var type = {
-	text: [],
-	timer: 0,
-	line: 0,
-	speed: 40,
-	cursor: "_",
-	cursor_visible: false,
-	break_timer: 0,
-	break_length: 9, //must be an odd number
-	end_of_line: false,
-	area: document.getElementById("logtext")
-}
-function typer() {
-	let { text, timer, line, speed, cursor, cursor_visible, break_timer, break_length, end_of_line, area } = type;
-
-	if (end_of_line) {
-		if (cursor_visible) {
-			area.innerHTML = area.innerHTML.slice(0,-1);
-		} else {
-			area.innerHTML += cursor;
-		}
-		type.cursor_visible = !cursor_visible;
-
-		if (break_timer >= break_length) {
-			type.break_timer = 0;
-			type.timer = 0;
-			type.line++;
-			type.end_of_line = false;
-			area.innerHTML += "<br />"
-
-			if (type.line >= text.length) { return }
-		} else {
-			type.break_timer++;
-		}
-
-		setTimeout(typer, speed)
-	}
-
-	if (timer <= text[line].length) {
-		area.innerHTML += text[line].charAt(timer);
-		type.timer++;
-		if (timer == text[line].length) { type.end_of_line = true }
-		setTimeout(typer, speed)
-	}
-}
+// browsing
 
 function key(e) {
 	let k = event.keyCode || event.which;
@@ -109,8 +60,6 @@ var links = {
 	freq: "freq"
 }
 
-var apples = 'apples';
-
 function search() {
 	let s = search_bar.value;
 	if (s.trim() == "") {
@@ -133,14 +82,14 @@ function search() {
 		return
 	}
 
-	frame.src = "websites/"+link_key+".html";
+	frame.src = "websites/"+link_key+"/index.html";
 }
 
 function load_update() {
 	let s = frame.contentWindow.location.href;
+	console.log(s);
 	s = s.split("/");
-	s = s[s.length-1];
-	s = s.replace(".html", "");
+	s = s[s.length-2];
 
 	if (links[s] != undefined) {
 		s = links[s];
@@ -162,4 +111,22 @@ function load_update() {
 
 function scroll_history() {
 	search_bar.value = search_history[search_history.length-1-sh_value];
+}
+
+// dialogs
+
+function log(t, link) {
+	if (current_screen != 'log') {
+		screen('log');
+	}
+	let textarea = document.getElementById("logtext");
+	textarea.innerHTML = t;
+}
+
+function browse(link) {
+	if (current_screen != 'browser') {
+		screen('browser');
+	}
+	search_bar.value = link;
+	search()
 }
