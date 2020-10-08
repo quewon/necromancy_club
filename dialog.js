afcdd1905850c3fd72bfea946802128451834cae
electriclantern <quewon.chin@gmail.com>
2020-10-08 10:04:24 +0900

    yeahhhh it works

--- dialog.js
+++ dialog.js
@@ -10,4 +10,6 @@ then again, it's been five years since the last meeting. taking a seat on a fami
 	,
 	LOG2:
 `another one, to be clear. it's as tragic as it is ironic. the necromancy club has never stopped haunting you, not after you moved away, not now that you're an adult. so it wasn't all that shocking, when you read <a onclick="browse('email.com')">the invitation</a>. almost like you had been anticipating something like this.`
-}
+};
+
+var admin_emails = ["rosebirdman@email.com"];
